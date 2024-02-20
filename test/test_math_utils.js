ef42f9573f51369ec5160ea36463d00ca2c37be6
Dave <50186003+thedave42@users.noreply.github.com>
2024-02-20 22:30:17 +0000

    Update test_math_utils.js with import statements and describe function

--- test/test_math_utils.js
+++ test/test_math_utils.js
@@ -1,7 +1,10 @@
 // Path: test/test_math_utils.js
-// Description: This file will contain tests for each of the functions in the math-utils folder.
+// Description: This file will contain tests for each of the functions in the ./math-utils folder.
 // It should use the Chai library for assertions.
 // It should use the Mocha library for running tests.
+// It should import the functions from the math-utils folder to test them.
+// It should use the describe function to group the tests for each function.
+// It should generate a variety of tests for each function, including edge cases.
 
 const math_utils_add = require('../math-utils/math_utils_add');
 const math_utils_pow = require('../math-utils/math_utils_pow');
